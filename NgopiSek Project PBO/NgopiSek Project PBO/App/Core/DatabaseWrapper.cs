06f68b9af26fa795f4d2af9c0ccb0590358f1101
Fauzul Akbar <149395810+fauzul91@users.noreply.github.com>
2024-11-13 15:40:50 +0700

    fix app

--- NgopiSek Project PBO/NgopiSek Project PBO/App/Core/DatabaseWrapper.cs
+++ NgopiSek Project PBO/NgopiSek Project PBO/App/Core/DatabaseWrapper.cs
@@ -8,13 +8,14 @@ using Npgsql;
 
 namespace NgopiSek_Project_PBO.App.Core
 {
-    internal class DatabaseWrapper
+    public class DatabaseWrapper
     {
+        // Properti credential database dan koneksinya
         private static readonly string DB_HOST = "localhost";
-        private static readonly string DB_DATABASE = "NgopiSek";// ubah nama data base kalian
+        private static readonly string DB_DATABASE = "NgopiSek";
         private static readonly string DB_USERNAME = "postgres";
-        private static readonly string DB_PASSWORD = "123";// ubah password postgre sql kalian
-        private static readonly string DB_PORT = "5432";// ubah port kalian jika beda
+        private static readonly string DB_PASSWORD = "123";
+        private static readonly string DB_PORT = "5432";
 
         public static NpgsqlConnection connection;
         public static NpgsqlCommand command;
@@ -22,9 +23,7 @@ namespace NgopiSek_Project_PBO.App.Core
         // Method open dan close Koneksi
         public static void openConnection()
         {
-            connection = new NpgsqlConnection($"Host={DB_HOST};" +
-                $"Username={DB_USERNAME};Password={DB_PASSWORD};" +
-                $"Database={DB_DATABASE};Port={DB_PORT}");
+            connection = new NpgsqlConnection($"Host={DB_HOST};Username={DB_USERNAME};Password={DB_PASSWORD};Database={DB_DATABASE};Port={DB_PORT}");
             connection.Open();
             command = new NpgsqlCommand();
             command.Connection = connection;
@@ -35,8 +34,8 @@ namespace NgopiSek_Project_PBO.App.Core
             connection.Close();
             command.Parameters.Clear();
         }
-                                                                                            
-        public static DataTable queryExecutor(string query, NpgsqlParameter[] parameters = null) //digunakan untuk open dan close koneksi data base dan mengisi data table
+
+        public static DataTable queryExecutor(string query, NpgsqlParameter[] parameters = null)
         {
             try
             {
@@ -58,19 +57,15 @@ namespace NgopiSek_Project_PBO.App.Core
                 throw new Exception(e.Message);
             }
         }
-
-        public static void commandExecutor(string query, NpgsqlParameter[] parameters = null)// method ini digunakan untuk INSERT, DELETE, AND UPDATE
-
+        public static void commandExecutor(string query, NpgsqlParameter[] parameters = null)
         {
             try
             {
                 openConnection();
                 command.CommandText = query;
-                if (parameters != null)
-                {
-                    command.Parameters.AddRange(parameters);
-                    command.Prepare();
-                }
+                command.Parameters.AddRange(parameters);
+                command.Prepare();
+                command.Parameters.Clear();
                 command.ExecuteNonQuery();
                 closeConnection();
             }
